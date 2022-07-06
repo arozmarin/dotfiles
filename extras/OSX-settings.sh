ed27a0c215b82480703a35c61b1c2faa300f9271
Ales Rozmarin <alesrozmarin@Aless-MacBook-Pro.local>
2022-07-06 21:59:32 +1000

    add scripts

---
+++ extras/OSX-settings.sh
@@ -0,0 +1,371 @@
+# OSX settings Ales Rozmarin
+#
+# Source: comming soon
+
+#!/bin/sh
+
+# Some things taken from here
+# https://github.com/mathiasbynens/dotfiles/blob/master/.osx
+# https://gist.github.com/philipashlock/de6a615e3b6958fc4ca1
+# https://github.com/dirtymouse/defaultswrite
+
+# Ask for the administrator password upfront
+
+###############################################################################
+# IMPORTANT!!!!! Allow terminal app Full disk access
+###############################################################################
+
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+echo "Ales Rozmarin personal settings for MacOSX"
+
+###############################################################################
+# General UI/UX
+###############################################################################
+
+# Menu bar sttings
+
+echo ""
+echo "Hide the Time Machine,icons"
+for domain in ~/Library/Preferences/ByHost/com.apple.systemuiserver.*; do
+  defaults write "${domain}" dontAutoLoad -array \
+    "/System/Library/CoreServices/Menu Extras/TimeMachine.menu"
+done
+# No more in Monterey
+#defaults write com.apple.systemuiserver menuExtras -array \
+#  "/System/Library/CoreServices/Menu Extras/Clock.menu"
+
+echo ""
+echo "Hide Siri, Volume, User and Bluetooth in menu bar"
+defaults write com.apple.Siri StatusMenuVisible -int 0
+defaults -currentHost write  com.apple.controlcenter Bluetooth -int 1
+defaults -currentHost write  com.apple.controlcenter UserSwitcher -int 24
+defaults -currentHost write com.apple.Spotlight MenuItemHidden -int 1
+defaults -currentHost write  com.apple.controlcenter NowPlaying -int 0
+
+#echo ""
+#echo "Disabling OS X Gate Keeper"
+#echo "(You'll be able to install any app you want from here on, not just Mac App Store apps)"
+#sudo spctl --master-disable
+#same result as spctl --master-disable and Gateskeeper won't check any file "Not recommended"
+#defaults write com.apple.LaunchServices LSQuarantine -bool false
+# remove quarantine for single file
+#xattr -d com.apple.quarantine /path-to-quarantined-file
+# Turn quarantine back on, use one of follow commands
+#defaults write com.apple.LaunchServices LSQuarantine -bool true
+#defaults delete com.apple.LaunchServices
+
+echo ""
+echo "Increasing the window resize speed for Cocoa applications"
+defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
+
+echo ""
+echo "Expanding the save panel by default"
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
+echo ""
+echo "Automatically quit printer app once the print jobs complete"
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+echo ""
+echo "Disabling automatic termination of inactive apps"
+defaults write NSGlobalDomain NSDisableAutomaticTermination -bool true
+
+echo ""
+echo "Saving to disk (not to iCloud) by default"
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+echo ""
+echo "Never go into computer sleep mode"
+systemsetup -setcomputersleep Off > /dev/null
+
+echo ""
+echo "Disable hibernation (speeds up entering sleep mode)"
+# Enforce system hibernation and evict FileVault keys from memory
+# instead of traditional sleep to memory.
+# Hibernation mode
+# 0: Disable hibernation (speeds up entering sleep mode)
+# 3: Copy RAM to disk so the system state can still be restored in case of a
+#    power failure.
+# 25: Force copying RAM to di sk always
+sudo pmset -a hibernatemode 0
+
+echo ""
+echo "Disable the sudden motion sensor as its not useful for SSDs"
+sudo pmset -a sms 0
+
+echo ""
+echo "Disable computer sleep and stop the display from shutting off"
+sudo pmset -a sleep 0
+sudo pmset -a displaysleep 0
+
+echo ""
+echo "Check for software updates daily, not just once per week"
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
+echo ""
+echo "Disable Resume system-wide"
+defaults write com.apple.systempreferences NSQuitAlwaysKeepsWindows -bool false
+
+###############################################################################
+# Trackpad, mouse, keyboard, Bluetooth accessories, and input
+###############################################################################
+
+echo ""
+echo "Increasing sound quality for Bluetooth headphones/headsets"
+defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
+
+echo ""
+echo "Disable beep on startup"
+sudo nvram StartupMute=%01
+
+echo ""
+echo "Enabling full keyboard access for all controls (e.g. enable Tab in modal dialogs)"
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+echo ""
+echo "Disabling press-and-hold for keys in favor of a key repeat"
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+echo ""
+echo "Swap F key with multimeda keys"
+defaults write -g com.apple.keyboard.fnState -bool true
+
+echo ""
+echo "Setting a blazingly fast keyboard repeat rate (ain't nobody got time fo special chars while coding!)"
+defaults write NSGlobalDomain InitialKeyRepeat -int 24
+defaults write NSGlobalDomain KeyRepeat -int 2
+
+
+echo ""
+echo "Setting trackpad & mouse speed to a reasonable number"
+defaults write -g com.apple.trackpad.scaling 2
+defaults write -g com.apple.mouse.scaling 2.5
+
+echo ""
+echo "Setting scroolbar Always on"
+defaults write -g AppleShowScrollBars -string "Always"
+
+echo ""
+echo "Trackpad: enable tap to click for this user and for the login screen"
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -int 1
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+sudo defaults write com.apple.AppleMultitouchTrackpad Clicking -int 1
+
+echo ""
+echo "Turn off keyboard illumination when computer is not used for 5 minutes"
+defaults write com.apple.BezelServices kDimTime -int 300
+
+###############################################################################
+# Screen
+###############################################################################
+
+echo ""
+echo "Requiring password immediately after sleep or screen saver begins"
+defaults write com.apple.screensaver askForPassword -bool true
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+echo ""
+echo "Enabling subpixel font rendering on non-Apple LCDs"
+defaults write NSGlobalDomain AppleFontSmoothing -int 2
+
+echo ""
+echo "Enable HiDPI display modes (requires restart)"
+sudo defaults write /Library/Preferences/com.apple.windowserver DisplayResolutionEnabled -bool true
+
+echo ""
+echo "Save screenshots to the downloads"
+defaults write com.apple.screencapture location -string "${HOME}/Downloads"
+
+echo ""
+echo "Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)"
+defaults write com.apple.screencapture type -string "png"
+
+###############################################################################
+# Finder
+###############################################################################
+
+echo ""
+echo "Showing icons for hard drives, servers, and removable media on the desktop"
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+
+echo ""
+echo "Showing all filename extensions in Finder by default"
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+echo ""
+echo "Showing status bar and path bar in Finder by default"
+defaults write com.apple.finder ShowStatusBar -bool true
+defaults write com.apple.finder ShowPathbar -bool true
+
+echo ""
+echo "Allowing text selection in Quick Look/Preview in Finder by default"
+defaults write com.apple.finder QLEnableTextSelection -bool true
+
+echo ""
+echo " show hidden files by default "
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+echo ""
+echo "Disabling the warning when changing a file extension"
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+echo ""
+echo "Use column view in all Finder windows by default"
+defaults write com.apple.finder FXPreferredViewStyle "Nlsv"
+
+echo ""
+echo "Avoiding the creation of .DS_Store files on network and USB volumes"
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+echo ""
+echo "Keep folders on top when sorting by name"
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+
+echo ""
+echo " When performing a search, search the current folder by default"
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+echo ""
+echo "Enabling snap-to-grid for icons on the desktop and in other icon views"
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+
+
+###############################################################################
+# Dock & Mission Control
+###############################################################################
+
+# Wipe all (default) app icons from the Dock
+# This is only really useful when setting up a new Mac, or if you don't use
+# the Dock to launch apps.
+defaults write com.apple.dock persistent-apps -array
+defaults write com.apple.dock recent-apps -array
+
+echo ""
+echo "Setting the icon size of Dock items to 36 pixels for optimal size/screen-realestate"
+defaults write com.apple.dock tilesize -int 36
+
+echo ""
+echo "Speeding up Mission Control animations and grouping windows by application"
+defaults write com.apple.dock expose-animation-duration -float 0.1
+defaults write com.apple.dock "expose-group-by-app" -bool true
+
+echo ""
+echo "Setting Dock to auto-hide and removing the auto-hiding delay"
+defaults write com.apple.dock autohide -int 1
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0
+defaults write com.apple.dock "mru-spaces" -int 0
+
+echo ""
+echo "Hot corners Setting"
+# Possible values:
+#  0: no-op
+#  2: Mission Control
+#  3: Show application windows
+#  4: Desktop
+#  5: Start screen saver
+#  6: Disable screen saver
+#  7: Dashboard
+# 10: Put display to sleep
+# 11: Launchpad
+# 12: Notification Center
+# 13: Lock Screen
+# 14: Quick Notes
+# Top left screen cornercc
+defaults write com.apple.dock wvous-tl-corner -int 0
+# Top right screen corner
+defaults write com.apple.dock wvous-tr-corner -int 0
+# Bottom left screen corner
+defaults write com.apple.dock wvous-bl-corner -int 13
+# Bottom right screen corner
+defaults write com.apple.dock wvous-br-corner -int 14
+
+echo ""
+echo "Disable all Dock animations when opening"
+defaults write com.apple.dock launchanim -int 0
+
+###############################################################################
+# Safari & WebKit
+###############################################################################
+
+echo ""
+echo "Hiding Safari's bookmarks bar by default"
+defaults write com.apple.Safari ShowFavoritesBar -bool false
+
+echo ""
+echo "Hiding Safari's sidebar in Top Sites"
+defaults write com.apple.Safari ShowSidebarInTopSites -bool false
+
+echo ""
+echo "Disabling Safari's thumbnail cache for History and Top Sites"
+defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
+
+echo ""
+echo "Enabling Safari's debug menu"
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+
+echo ""
+echo "Making Safari's search banners default to Contains instead of Starts With"
+defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false
+
+echo ""
+echo "Allow hitting the Backspace key to go to the previous page in history"
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2BackspaceKeyNavigationEnabled -bool true
+
+echo ""
+echo "Enabling the Develop menu and the Web Inspector in Safari"
+defaults write com.apple.Safari.SandboxBroker ShowDevelopMenu -bool true
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
+
+echo ""
+echo "Prevent Safari from opening ‘safe’ files automatically after downloading"
+defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
+
+echo ""
+echo "Show compact tab layout"
+defaults write com.apple.Safari ShowStandaloneTabBar -bool false
+
+###############################################################################
+# Terminal
+###############################################################################
+
+echo ""
+echo "Enabling UTF-8 ONLY in Terminal.app and setting the Pro theme by default"
+defaults write com.apple.terminal StringEncodings -array 4
+defaults write com.apple.Terminal "Default Window Settings" -string "Pro"
+defaults write com.apple.Terminal "Startup Window Settings" -string "Pro"
+
+# Specify the preferences directory
+#defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.config/iterm2"
+# Tell iTerm2 to use the custom preferences in the directory
+#defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+
+###############################################################################
+# Time Machine
+###############################################################################
+
+echo ""
+echo "Preventing Time Machine from prompting to use new hard drives as backup volume"
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+echo ""
+echo "Disabling local Time Machine backups"
+hash tmutil &> /dev/null && sudo tmutil disable
+
+###############################################################################
+# Kill affected applications
+###############################################################################
+
+echo "Done!"
